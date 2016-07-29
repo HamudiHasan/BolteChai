8dbd5d40f603356361f00b70534c7581bb0c3b69
HamudiHasan <hhsonet@gmail.com>
2016-07-29 17:12:08 +0600

    Fisrt commit from me

--- BolteChai/app/src/main/java/com/aims/boltechai/model/ActivityItem.java
+++ BolteChai/app/src/main/java/com/aims/boltechai/model/ActivityItem.java
@@ -1,17 +1,31 @@
 package com.aims.boltechai.model;
 
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by Zakir on 28/07/2016.
  */
+
+@Table(name = "ActivityItem")
 public class ActivityItem {
 
     // TODO: 28/07/2016 for sonet
     // make this class a model table for active android
 
+    @Column(name = "acttivityId")
     public int acttivityId;
+
+    @Column(name = "acttivityImage")
     public String acttivityImage;
+
+    @Column(name = "acttivityTitle")
     public String acttivityTitle;
+
+    @Column(name = "acttivityAudio")
     public String acttivityAudio;
+
+    @Column(name = "acttivityCategory")
     public int acttivityCategory;
 
 }
