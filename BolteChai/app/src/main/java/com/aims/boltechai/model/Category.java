7a912d5601f0a58bf33933982764cba2a78ea531
HamudiHasan <hhsonet@gmail.com>
2016-07-29 17:28:24 +0600

    Fisrt commit from me

--- BolteChai/app/src/main/java/com/aims/boltechai/model/Category.java
+++ BolteChai/app/src/main/java/com/aims/boltechai/model/Category.java
@@ -1,14 +1,23 @@
 package com.aims.boltechai.model;
 
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by Zakir on 17/02/2016.
  */
+
+@Table(name = "Category")
 public class Category {
     // TODO: 28/07/2016 for sonet
     // make this class a model table for active android
 
-
+    @Column(name = "categoryId")
     public int categoryId;
+
+    @Column (name = "categoryImage")
     public String categoryImage;
+
+    @Column (name = "categoryTitle")
     public String categoryTitle;
 }
